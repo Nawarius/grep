81f7aa55e4800f66ac07262048cdc3f6b3863169
Artem Ermakov <nawarius1993@gmail.com>
2022-07-12 07:05:43 +0400

    Adds: Get args from command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect::<Vec<String>>();
+
+    let query: &str = &args[1];
+    let filename: &str = &args[2];
+
+    println!("Searching string is: {}", query);
+    println!("Filename is: {}", filename);
+
 }
