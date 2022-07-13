7e1d5d5eafc78e7d549ad4770f631b9d0fb794cf
Artem Ermakov <nawarius1993@gmail.com>
2022-07-13 18:57:48 +0400

    Adds: Ability change sensitive/insensitive search by env variable. Refactor: Errors to err thread. Tests: Add test for insensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,58 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
 pub struct Config {
     pub query: String,
-    pub filename: String
+    pub filename: String,
+    pub ignore_case: bool
 }
 
 impl Config {
     pub fn build (args: &[String]) -> Result<Self, &'static str> {
         if args.len() < 3 { return Err("Not enough arguments") }
 
-        Ok(Config { query: args[1].clone(), filename: args[2].clone() })
+        Ok(Config { 
+            query: args[1].clone(), 
+            filename: args[2].clone(),
+            ignore_case: env::var("IGNORE_CASE").is_ok() 
+        })
     }
 }
 
 pub fn run (config: Config) -> Result<(), Box<dyn Error>> {
     let content: String = fs::read_to_string(config.filename)?;
     
-    for line in search(&config.query, &content) {
+    let results = if config.ignore_case {
+        search_insensitive(&config.query, &content)
+    } else {
+        search_sensitive(&config.query, &content)
+    };
+
+    for line in results {
         println!("{}", line)
     }
 
     Ok(())
 }
 
-pub fn search <'a, 'b> (query: &'a str, contents: &'b str) -> Vec<&'b str> {
+pub fn search_sensitive <'a, 'b> (query: &'a str, contents: &'b str) -> Vec<&'b str> {
     let mut results: Vec<&'b str> = Vec::new();
 
     for line in contents.lines() {
         if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_insensitive <'a, 'b> (query: &'a str, contents: &'b str) -> Vec<&'b str> {
+    let mut results: Vec<&'b str> = Vec::new();
+    let query = query.to_lowercase();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
@@ -40,13 +64,29 @@ mod tests {
     use super::*;
 
     #[test] 
-    fn one_result () {
+    fn case_sensitive () {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
         
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+        assert_eq!(vec!["safe, fast, productive."], search_sensitive(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive () {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_insensitive(query, contents)
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect::<Vec<String>>();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Fail parsing command line arguments: {}", err);
+        eprintln!("Fail parsing command line arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(err) = grep::run(config) {
-        println!("Application error: {}", err);
+        eprintln!("Application error: {}", err);
         process::exit(1);
     }
     
